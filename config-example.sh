5e49ef30538cfa89d73a74353a145943fa0ad2fe
root <root@mail.machineitservices.com>
2017-12-31 10:01:24 -0500

    Updating config-example.sh

--- config-example.sh
+++ config-example.sh
@@ -1,31 +1,62 @@
 ############### START CONFIG ###############
 
-hostnameaddr=cf.example.com			## Check IP Address of This Hostname. I recommend making one specifically for Cloudflare
+## Check IP Address of This Hostname. I recommend making one specifically for Cloudflare
+hostnameaddr=cf.example.com
 
-zones=(example.com example1.com example2.com)	## Zones to update. Listed in array surrounded by paranthesis in the format: (example1.com example2.com ...) Remember array values start at 0
+## Zones to update. Listed in array surrounded by paranthesis in the format: (example1.com example2.com ...) Remember array values start at 0
+zones=(example.com example1.com example2.com)
 
-############# User Credentials #############
+############# Credentials #############
 
-user1_creds=("user1@example.com" "user1APIkey")	## First Set of Credentials listed in array in the format: (user@name.com zone_global_api_key)
-user1_credzone=(0 1)				## Specify Zones that utilize the First Set of Credentials in array in the format: (0 1 ...)
-user2_creds=("user2@example2.com" "user2APIkey") ## Second Set of Credentials listed in array in the format: (user@name.com zone_global_api_key)
-user2_credzone=(2)				 ## Specify Zones that utilize the Second Set of Credentials in array in the format: (0 1 ...)
+## First Set of Credentials listed in array in the format: (user@name.com zone_global_api_key)
+user1_creds=("user1@example.com" "user1APIkey")
 
-############## Custom Records ##############
+## Specify Zones that utilize the First Set of Credentials in array in the format: (0 1 ...)
+user1_credzone=(0 1)
 
-custom1_records=(cf)				## First Set of Records listed in array in the format: ("" cf mail ...) I recommend including "hostnameaddr" above
-custom1_zones=(0)				## Specify Zones containing the aforementioned records to be updated in array in the format: (0 1 ...)
-custom1_proxied=no				## Specify whether records updated will have proxy enabled or disabled. Use either "yes" or "no" without quotes
-custom2_records=("")				## Second Set of Records listed in array in the format: ("" ns1 www ...)
-custom2_zones=(0 1 2)				## Specify Zones containing the aforementioned records to be updated in array in the format: (0 1 ...)
-custom2_proxied=yes				## Specify whether records updated will have proxy enabled or disabled. Use either "yes" or "no" without quotes
+
+## Second Set of Credentials listed in array in the format: (user@name.com zone_global_api_key)
+user2_creds=("user2@example2.com" "user2APIkey")
+
+## Specify Zones that utilize the Second Set of Credentials in array in the format: (0 1 ...)
+user2_credzone=(2)
+
+############## Records ##############
+
+## First Set of Records listed in array in the format: ("" cf mail ...) I recommend including "hostnameaddr" above
+custom1_records=(cf)
+
+## Specify Zones containing the aforementioned records to be updated in array in the format: (0 1 ...)
+custom1_zones=(0)
+
+## Specify whether records updated will have proxy enabled or disabled. Use either "yes" or "no" without quotes
+custom1_proxied=no
+
+
+## Second Set of Records listed in array in the format: ("" ns1 ...)
+custom2_records=("")
+
+## Specify Zones containing the aforementioned records to be updated in array in the format: (0 1 ...)
+custom2_zones=(0 1 2)
+
+## Specify whether records updated will have proxy enabled or disabled. Use either "yes" or "no" without quotes
+custom2_proxied=yes
 
 # If you add custom variables above (i.e. user3... or custom3...), you will also have to change the values below.
 
-user_creds_num=2				## Number of Sets of Credentials Above
-custom_records_num=2				## Number of Sets of Records Above
-updatedir=/your/working/directory		## Directory to run the script from
-ids_dir=ids					## Subdirectory to save API Identifier files for faster updates in the future
-force=no					## Force update of records. Recommended if records modified, updated, removed, or added.
+## Number of Sets of Credentials Above
+user_creds_num=2
+
+## Number of Sets of Records Above
+custom_records_num=2
+
+## Subdirectory to save API Identifiers for faster updates in the future
+ids_dir=ids
+
+## Directory to run the script from. Uncomment to override (Normally set to script location)
+#updatedir=/your/working/directory
+
+## Force update of records. Recommended if adding a new domain. Uncomment when needed.
+#force=no
 
 ################ END CONFIG ################
